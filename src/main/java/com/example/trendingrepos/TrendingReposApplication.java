05f1be1fa1192b6beac8bd0c72429a71c4aee59c
zel <zouhirelghai@gmail.com>
2021-08-20 00:27:41 +0100

    initiate project

---
+++ src/main/java/com/example/trendingrepos/TrendingReposApplication.java
@@ -0,0 +1,13 @@
+package com.example.trendingrepos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendingReposApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrendingReposApplication.class, args);
+    }
+
+}
